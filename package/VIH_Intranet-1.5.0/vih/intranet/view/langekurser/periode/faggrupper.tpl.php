882d99813da233b30bab998776df2244731e1911
Lars Olesen <lars@intraface.dk>
2010-01-27 16:48:47 +0000

    Deleted stuff that should not have been versioned.

--- package/VIH_Intranet-1.5.0/vih/intranet/view/langekurser/periode/faggrupper.tpl.php
+++
@@ -1,9 +0,0 @@
-<table>
-<?php foreach ($faggrupper as $gruppe): ?>
-    <tr>
-        <td><a href="<?php e(url($gruppe->getId())); ?>"><?php e($gruppe->getName()); ?></a></td>
-        <td><?php e($gruppe->getDescription()); ?></td>
-        <td><a href="<?php e(url($gruppe->getId() . '/edit')); ?>">Ret</a></td>
-    </tr>
-<?php endforeach; ?>
-</table>
