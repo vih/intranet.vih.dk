61530c66b77d47baf1b536abb2daf6699a1a70c9
Lars Olesen <lars@intraface.dk>
2010-04-17 08:37:54 +0000

    Fixed #150

--- src/VIH/Intranet/Controller/Langekurser/Tilmeldinger/Edit.php
+++ src/VIH/Intranet/Controller/Langekurser/Tilmeldinger/Edit.php
@@ -11,6 +11,8 @@ class VIH_Intranet_Controller_Langekurser_Tilmeldinger_Edit extends k_Component
 
     function getForm()
     {
+        $date_options = array('minYear' => date('Y') - 10, 'maxYear' => date('Y') + 5);
+
         if ($this->form) {
             return $this->form;
         }
@@ -21,28 +23,28 @@ class VIH_Intranet_Controller_Langekurser_Tilmeldinger_Edit extends k_Component
             $kurser[$kursus->get('id')] = $kursus->getKursusNavn();
         }
 
-        $form = new HTML_QuickForm('tilemdling', 'POST', $this->url());
+        $form = new HTML_QuickForm('tilmelding', 'POST', $this->url());
         $form->addElement('hidden', 'id');
         $form->addElement('header', null, 'Kursus');
         $form->addElement('select', 'kursus_id', 'Kursus', $kurser);
         $form->addElement('header', null, 'Navn og adresse');
-        $form->addElement('text', 'vaerelse', 'Værelse');
+        $form->addElement('text', 'vaerelse', 'VÃ¦relse');
         $form->addElement('text', 'navn', 'Navn');
         $form->addElement('text', 'adresse', 'Adresse');
         $form->addElement('text', 'postnr', 'Postnummer');
         $form->addElement('text', 'postby', 'Postby');
         $form->addElement('text', 'cpr', 'Cpr-nummer');
         $form->addElement('text', 'telefonnummer', 'Telefonnummer');
-        $form->addElement('text', 'kommune', 'Bopælskommune');
+        $form->addElement('text', 'kommune', 'BopÃ¦lskommune');
         $form->addElement('text', 'nationalitet', 'Nationalitet');
         $form->addElement('text', 'email', 'E-mail');
 
         foreach ($tilmelding->sex AS $key=>$value) {
             $radio[] = &HTML_QuickForm::createElement('radio', null, null, $value, $key);
         }
-        $form->addGroup($radio, 'sex', 'Køn');
+        $form->addGroup($radio, 'sex', 'KÃ¸n');
 
-        $form->addElement('header', null, 'Nærmeste pårørende - hvem skal vi rette henvendelse til ved sygdom');
+        $form->addElement('header', null, 'NÃ¦rmeste pÃ¥rÃ¸rende - hvem skal vi rette henvendelse til ved sygdom');
         $form->addElement('text', 'kontakt_navn', 'Navn');
         $form->addElement('text', 'kontakt_adresse', 'Adresse');
         $form->addElement('text', 'kontakt_postnr', 'Postnummer');
@@ -50,7 +52,7 @@ class VIH_Intranet_Controller_Langekurser_Tilmeldinger_Edit extends k_Component
         $form->addElement('text', 'kontakt_telefon', 'Telefon');
         $form->addElement('text', 'kontakt_arbejdstelefon', 'Arbejdstelefon');
         $form->addElement('text', 'kontakt_email', 'E-mail');
-        $form->addElement('header', null, 'Hvordan er din uddannelsesmæssige baggrund?');
+        $form->addElement('header', null, 'Hvordan er din uddannelsesmÃ¦ssige baggrund?');
         foreach ($tilmelding->uddannelse AS $key=>$value) {
             $udd[] = &HTML_QuickForm::createElement('radio', null, null, $value, $key);
         }
@@ -60,28 +62,28 @@ class VIH_Intranet_Controller_Langekurser_Tilmeldinger_Edit extends k_Component
             $bet[] = &HTML_QuickForm::createElement('radio', null, null, $value, $key);
         }
         $form->addGroup($bet, 'betaling', 'Betaling');
-        $form->addElement('header', null, 'Besked til Vejle Idrætshøjskole');
-        $form->addElement('textarea', 'besked', 'Er der andet vi bør vide?');
+        $form->addElement('header', null, 'Besked til Vejle IdrÃ¦tshÃ¸jskole');
+        $form->addElement('textarea', 'besked', 'Er der andet vi bÃ¸r vide?');
         $form->addElement('textarea', 'tekst_diplom', 'Tekst til diplomet');
         $form->addElement('header', null, 'Termin');
         $form->addElement('text', 'ugeantal', 'Ugeantal');
-        $form->addElement('date', 'dato_start', 'Startdato');
-        $form->addElement('date', 'dato_slut', 'Slutdato');
+        $form->addElement('date', 'dato_start', 'Startdato', $date_options);
+        $form->addElement('date', 'dato_slut', 'Slutdato', $date_options);
         $form->addElement('header', null, 'Priser');
         $form->addElement('text', 'pris_tilmeldingsgebyr', 'Tilmeldingsgebyr');
         $form->addElement('text', 'pris_uge', 'Ugepris');
         $form->addElement('text', 'pris_materiale', 'Materialer');
-        $form->addElement('text', 'pris_noegledepositum', 'Nøgledepositum');
+        $form->addElement('text', 'pris_noegledepositum', 'NÃ¸gledepositum');
         $form->addElement('text', 'pris_rejsedepositum', 'Rejsedepositum');
         $form->addElement('text', 'pris_rejserest', 'Rejserestpris');
         $form->addElement('text', 'pris_rejselinje', 'Rejselinjepris');
-        $form->addElement('header', null, 'Støtte');
-        $form->addElement('text', 'elevstotte', 'Elevstøtte');
-        $form->addElement('text', 'ugeantal_elevstotte', 'Elevstøtte antal uger');
-        $form->addElement('text', 'kompetencestotte', 'Kompetencestøtte');
-        $form->addElement('text', 'statsstotte', 'Indvandrerstøtte');
-        $form->addElement('text', 'kommunestotte', 'Kommunestøtte');
-        $form->addElement('text', 'aktiveret_tillaeg', 'Aktiveret tillæg');
+        $form->addElement('header', null, 'StÃ¸tte');
+        $form->addElement('text', 'elevstotte', 'ElevstÃ¸tte');
+        $form->addElement('text', 'ugeantal_elevstotte', 'ElevstÃ¸tte antal uger');
+        $form->addElement('text', 'kompetencestotte', 'KompetencestÃ¸tte');
+        $form->addElement('text', 'statsstotte', 'IndvandrerstÃ¸tte');
+        $form->addElement('text', 'kommunestotte', 'KommunestÃ¸tte');
+        $form->addElement('text', 'aktiveret_tillaeg', 'Aktiveret tillÃ¦g');
         $form->addElement('header', null, 'Afbrudt ophold');
         $form->addElement('text', 'pris_afbrudt_ophold', 'Ekstra pris');
         $form->addElement('submit', null, 'Gem');
@@ -90,8 +92,8 @@ class VIH_Intranet_Controller_Langekurser_Tilmeldinger_Edit extends k_Component
         $form->applyFilter('__ALL__', 'strip_tags');
 
         $form->addRule('id', 'Tilmeldingen skal have et id', 'numeric');
-        $form->addRule('kursus_id', 'Du skal vælge et kursus', 'required');
-        $form->addRule('kursus_id', 'Du skal vælge et kursus', 'numeric');
+        $form->addRule('kursus_id', 'Du skal vÃ¦lge et kursus', 'required');
+        $form->addRule('kursus_id', 'Du skal vÃ¦lge et kursus', 'numeric');
         $form->addRule('navn', 'Du skal skrive et navn', 'required');
         $form->addRule('adresse', 'Du skal skrive en adresse', 'required');
         $form->addRule('postnr', 'Postnummer', 'required');
@@ -109,8 +111,8 @@ class VIH_Intranet_Controller_Langekurser_Tilmeldinger_Edit extends k_Component
         $form->addRule('kontakt_arbejdstelefon', 'Du har ikke skrevet et nummer under arbejdstelefon', 'required');
         $form->addRule('kontakt_email', 'Du har ikke skrevet en gyldig kontakte-mail', 'email');
 
-        $form->addGroupRule('uddannelse', 'Du skal vælge din uddannelsesmæssige baggrund', 'required', null);
-        $form->addGroupRule('betaling', 'Du skal vælge, hvordan du betaler', 'required', null);
+        $form->addGroupRule('uddannelse', 'Du skal vÃ¦lge din uddannelsesmÃ¦ssige baggrund', 'required', null);
+        $form->addGroupRule('betaling', 'Du skal vÃ¦lge, hvordan du betaler', 'required', null);
 
         return ($this->form = $form);
     }
@@ -192,5 +194,4 @@ class VIH_Intranet_Controller_Langekurser_Tilmeldinger_Edit extends k_Component
         }
 
     }
-
 }
