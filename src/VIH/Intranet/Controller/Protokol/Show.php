faba21b744ee75735cffd1513c188a44eb05f725
Lars Olesen <lars@intraface.dk>
2010-06-19 16:48:11 +0200

    Moved the delete action to the show controller to get closer to best practise for konstrukt.

--- src/VIH/Intranet/Controller/Protokol/Show.php
+++ src/VIH/Intranet/Controller/Protokol/Show.php
@@ -1,10 +1,17 @@
 <?php
 class VIH_Intranet_Controller_Protokol_Show extends k_Component
 {
-    function map($name)
+    private $db;
+
+    function __construct(DB_common $db)
     {
-        if ($name == 'delete') {
-            return 'VIH_Intranet_Controller_Protokol_Delete';
-        }
+        $this->db = $db;
+    }
+
+    function renderHtml()
+    {
+        $res = $this->db->query('DELETE FROM langtkursus_tilmelding_protokol_item WHERE id = ' . (int)$this->context->name());
+
+        return new k_SeeOther($this->url('../../'));
     }
 }
