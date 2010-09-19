fe91ff9d3ea3b1364f471733092a1cb093e41a53
Lars Olesen <lars@intraface.dk>
2010-09-19 12:46:59 +0200

    Added missing index.tpl.php

---
+++ src/VIH/Intranet/view/index.tpl.php
@@ -0,0 +1,6 @@
+<form action="<?php e(url()); ?>" method="POST">
+    <label>Hvad sker der lige nu? (Twitter)<br />
+        <input name="twitter" type="text" maxlength="160" style="font-size: 2em;" />
+    </label>
+    <input type="submit" value="Gem" />
+</form>
