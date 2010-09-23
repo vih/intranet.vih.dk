b5201b73a4038bc78f37a574143e1e240ad25fa6
Lars Olesen <lars@intraface.dk>
2010-09-23 17:16:22 +0200

    Fixed bugs with subjects and made sure the doctrine conn is utf8

--- src/VIH/Intranet/Controller/Langekurser/Tilmeldinger/Fag.php
+++ src/VIH/Intranet/Controller/Langekurser/Tilmeldinger/Fag.php
@@ -1,7 +1,8 @@
 <?php
-class VIH_Intranet_Controller_Langekurser_Tilmeldinger_Fag extends VIH_Controller_LangtKursus_Login_Fag
+class VIH_Intranet_Controller_Langekurser_Tilmeldinger_Fag extends k_Component
 {
     protected $template;
+    protected $doctrine;
 
     function __construct(k_TemplateFactory $template, Doctrine_Connection_Common $doctrine)
     {
@@ -11,10 +12,88 @@ class VIH_Intranet_Controller_Langekurser_Tilmeldinger_Fag extends VIH_Controlle
 
     function renderHtml()
     {
-        $this->document->setTitle($this->getRegistration()->get('navn').' fag på '.$this->getRegistration()->getKursus()->getKursusNavn());
-        $this->document->addOption('Tilmeldingen', $this->url('../'));
-        $this->document->addOption('Diplom (pdf)', $this->url('../diplom'));
-    	return parent::GET();
+        $this->document->setTitle('Tilmelding: Vælg fag');
+
+        $tilmelding = $this->getRegistration();
+
+        $periods = $this->getPeriods();
+
+        $data = array('periods' => $periods,
+                      'tilmelding' => $tilmelding);
+
+        $tpl = $this->template->create('langekurser/tilmelding/fag');
+        return $tpl->render($this, $data);
+    }
+
+    function postForm()
+    {
+        if (!$this->validate()) {
+            throw new Exception('Du skal vælge et af hvert fag');
+        }
+
+        $registration = Doctrine::getTable('VIH_Model_Course_Registration')->findOneById($this->getRegistration()->getId());
+
+        $current_subjects = Doctrine::getTable('VIH_Model_Course_Registration_Subject')->findByRegistrationId($this->getRegistration()->getId());
+
+        foreach ($current_subjects as $subj) {
+            $subj->delete();
+        }
+        /*
+        $subjects = array();
+        foreach ($registration->Subjects as $subject) {
+            $subjects[] = $subject->getId();
+        }
+        if (!empty($subjects)) {
+            try {
+                $registration->unlink('Subjects', $subjects);
+            } catch (Doctrine_Query_Exception $e) {
+
+            }
+        }
+        */
+
+        if ($this->body('subject')) {
+            $input = $this->body();
+            foreach ($this->body('subject') as $key => $value) {
+                $registrationsubject = new VIH_Model_Course_Registration_Subject;
+                $registrationsubject->period_id = $input['subjectperiod'][$key];
+                $registrationsubject->registration_id = $this->getRegistration()->getId();
+                $registrationsubject->subject_id = $value;
+                $registrationsubject->subjectgroup_id = $input['subjectgroup'][$key];
+                $registrationsubject->save();
+            }
+        }
+
+        //$registration->save();
+
+        return new k_SeeOther($this->getRedirectUrl());
+    }
+
+    function getPeriods()
+    {
+        $tilmelding = new VIH_Model_LangtKursus_OnlineTilmelding($this->context->name());
+        return Doctrine::getTable('VIH_Model_Course_Period')->findByCourseId($tilmelding->getKursus()->getId());
+    }
+
+    function getRedirectUrl()
+    {
+        return $this->url();
+    }
+
+    /**
+     * This method should validate whether the correct number of subjects has
+     * been chosen i the different periods and subjectgroups.
+     */
+    function validate()
+    {
+        // @hack FIXME @todo
+        return true;
+        /*
+        if (empty($this->POST['subject'])) {
+            return false;
+        }
+        return true;
+        */
     }
 
     function getRegistration()
--- src/VIH/Intranet/view/langekurser/tilmelding/fag.tpl.php
+++ src/VIH/Intranet/view/langekurser/tilmelding/fag.tpl.php
@@ -1,17 +1,31 @@
-<form action="<?php echo $this->url(); ?>" method="post" id="fag">
-<?php $period = Doctrine::getTable('VIH_Model_Course_Period')->findByCourseId($tilmelding->Course->getId()); ?>
-<?php foreach ($period as $p) { ?>
-    <h2><?php e($p->getName()); ?></h2>
+<?php
+    $chosen = array();
+    $registration = Doctrine::getTable('VIH_Model_Course_Registration')->findOneById($tilmelding->getId());
+    $ch_subj = Doctrine::getTable('VIH_Model_Course_Registration_Subject')->findByRegistrationId($tilmelding->getId());
+
+    foreach ($ch_subj as $subj) {
+    	$chosen[$subj->period_id . $subj->subject_id . $subj->subjectgroup_id] = $subj->subject_id;
+    }
+?>
+
+<form action="<?php e(url()); ?>" method="post" id="fag">
+<?php $i = 0; foreach ($periods as $p): ?>
+    <h2 class="periode"><?php e($p->getName()); ?></h2>
     <?php $subjectgroups = Doctrine::getTable('VIH_Model_Course_SubjectGroup')->findByPeriodId($p->getId()); ?>
-    <?php foreach ($subjectgroups as $grp) { ?>
+    <?php foreach ($subjectgroups as $grp): ?>
+        <?php if (!$grp->isElectiveCourse()) continue; ?>
         <fieldset>
         <legend><?php e($grp->getName()); ?></legend>
-        <?php foreach ($grp->Subjects as $subj) { ?>
-            <input type="checkbox" name="subjects[]" value="<?php e($subj->getId()); ?>" <?php if (in_array($subj->getId(), $chosen)) echo ' checked="checked"' ?>/> <label for=""><?php e($subj->getName()); ?></label>
-        <?php } ?>
+        <?php foreach ($grp->Subjects as $subj): ?>
+            <input type="checkbox" name="subject[<?php e($i); ?>]" value="<?php e($subj->getId()); ?>"<?php if (isset($chosen[$p->getId() . $subj->getId() . $grp->getId()]) AND $chosen[$p->getId() . $subj->getId() . $grp->getId()] == $subj->getId()) echo ' checked="checked"' ?>>
+            <input type="hidden" name="subjectperiod[<?php e($i); ?>]" value="<?php e($p->getId()); ?>" />
+            <input type="hidden" name="subjectgroup[<?php e($i); ?>]" value="<?php e($grp->getId()); ?>" />
+            <label for=""><?php e($subj->getName()); ?></label>
+        <?php ++$i; ?>
+        <?php endforeach; ?>
         </fieldset>
-    <?php } ?>
-<?php } ?>
-</fieldset>
+    <?php endforeach; ?>
+<?php endforeach; ?>
+
 <p><input type="submit" id="submit" value="Videre" /></p>
 </form>
