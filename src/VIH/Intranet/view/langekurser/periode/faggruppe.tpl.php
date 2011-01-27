b727c75824212bd7103c2bc7019379ef9ff979f3
Sune Jensen <sune@asterix.site>
2011-01-27 22:04:04 +0100

    Added faggruppe when adding fag to perioder. Fix bug in adding fag to periode

--- src/VIH/Intranet/view/langekurser/periode/faggruppe.tpl.php
+++ src/VIH/Intranet/view/langekurser/periode/faggruppe.tpl.php
@@ -1,10 +1,15 @@
 <form method="post" action="<?php e(url('./')); ?>">
 <table>
-<?php foreach ($fag as $f): ?>
+<?php foreach ($faggrupper as $gruppe): ?>
     <tr>
-        <td><input type="checkbox" name="fag[]" value="<?php e($f->getId()); ?>" <? if(in_array($f->getId(), $chosen)) echo 'checked="checked"'; ?>/></td>
-        <td><?php e($f->getName()); ?></td>
+        <th colspan="2"><?php e($gruppe->get('navn')); ?></th>
     </tr>
+    <?php foreach ($fag[$gruppe->get('id')] AS $f): ?>
+        <tr>
+            <td><input type="checkbox" name="fag[]" value="<?php e($f->getId()); ?>" <? if(in_array($f->getId(), $chosen)) echo 'checked="checked"'; ?>/></td>
+            <td><?php e($f->getName()); ?></td>
+        </tr>
+    <?php endforeach; ?>    
 <?php endforeach; ?>
 </table>
 <input type="submit" value="Send" />
